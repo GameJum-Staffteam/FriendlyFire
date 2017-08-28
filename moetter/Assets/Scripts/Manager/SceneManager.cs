4d2b7205e1c79209df7c4cd3da79a64bf6a6fede
yutatakatsu <vocaloid_asax@yahoo.co.jp>
2017-08-28 18:22:59 +0900

    【SceneManager】シーンの追加　フェードインアウトの実装

---
+++ moetter/Assets/Scripts/Manager/SceneManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+
+public enum SCENES
+{
+    TITLE,
+    GAME,
+    RESULT,
+}
+namespace Scene
+{
+    static public class SceneManager
+    {
+
+
+        static public void SceneMove(SCENES NextScene)
+        {
+
+            FadeManager.instance.LoadScene((int)NextScene, 1.0f);
+
+        }
+    }
+}
